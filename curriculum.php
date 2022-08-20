3dabf1b26ce6d77c3c8b63dd998d8cb81f765f48
lual25 <79921705+lual25@users.noreply.github.com>
2022-08-19 20:46:21 -0500

    Initial commit

---
+++ curriculum.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <h1>Curriculum Vitae</h1>
+    </head>
+    <body>
+        <!--Datos personales-->
+        <h2>Luis Alfredo Pérez Hidalgo</h2>
+        <img src="20200910_104454.jpg " width="250"
+        height="341">
+        <h2>Personal Data</h2>
+        <h4>Phone number:</h4>
+        <p>3323958884</p>
+        <h4>Age:</h4>
+        <p>23 years old</p>
+        <h4>Social Media:</h4>
+        <ul>
+            <li>E-mail: lual.escolar@gmail.com</li>
+            <li>Twitter: @Lual_P</li>
+        </ul>
+        <h4>Nacionality:</h4>
+        <p>Mexican</p>
+        <h2>Academic</h2>
+        <!--Academic-->
+        <h4>Current grade</h4>
+        <p> 9 Universidad de Guadalajara</p>
+        <!--abilities-->
+        <h2>Abilities</h2>
+        <ul>
+            <li>Puntuality       95%</li>
+            <li>Responsability   90%</li>
+            <li>Team work        85%</li>
+            <li>Perseverance     90%</li>
+        </ul>
+        <!--Experience-->
+        <h2>Project experience</h2>
+        <p>Mateayuda: Web software oriented to children's mathematical learning</p>
+        <p>Goblins: Goblins: A RPG game made on Unity</p>
+        <!--Professional interests-->
+        <h2>Professional interests</h2>
+        <ul>
+            <li>Programing</li>
+            <li>Web Design</li>
+            <li>Game development</li>
+            <li>3D modeling</li>
+
+        </ul>
+        <!--Software-->
+        <h2>Software</h2>
+        <ul>
+            <li>Unity:           60%</li>
+            <li>Blender          50%</li>
+            <li>Visual Studio    75%</li>
+        </ul>
+        <!--Language-->
+        <h2>Language</h2>
+        <ul>
+            <li>Spanish  98%</li>
+            <li>English  75%</li>
+        </ul>
+        <!--References-->
+        <h2>References</h2>
+        <p>Ana Cristina H. 3322114560</p>
+        <p>Alfredo O. 3369874125</p>
+    </body>
+</html>
